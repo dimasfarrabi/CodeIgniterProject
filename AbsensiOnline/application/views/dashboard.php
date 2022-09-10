dd1bb48ad5324ede247f5783739782f73005a5ac
Dimas Farrabi <111451164+dimasfarrabi@users.noreply.github.com>
2022-09-10 21:17:22 +0700

    Add files via upload

---
+++ AbsensiOnline/application/views/dashboard.php
@@ -0,0 +1,5 @@
+<div class="jumbotron text-white" style="background-color:#393939">
+    <h4 class="my-0">Selamat datang di</h4>
+    <h1 class="display-4 my-0">Aplikasi Absensi Online</h1>
+    <hr class="my-4">
+</div>
